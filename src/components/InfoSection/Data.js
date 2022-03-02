9c58af1002af58cbd6c977e0fd2194bb03df8154
Onesio <Onesiojr1@gmail.com>
2022-03-01 23:02:03 -0300

    Criando paginas secundarias

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Sobre Nós',
+    headLine: 'A Historia do melhor restaurante japones da sua região',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra fermentum nulla,quis tincidunt ante ultrices eget. Vivamus vitae neque at orci congue faucibus vitae sit amet velit. Nam blandit tristique sem id lacinia. Etiam.',
+    buttonLabel: 'Seja um de Nós',
+    imgStart: false,
+    img: require('../../images/svg1.svg').default,
+    alt: 'sushi',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+export const homeObjTwo = {
+    id: 'Stores',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Nossas Lojas',
+    headLine: 'A Historia do melhor restaurante japones da sua região',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra fermentum nulla,quis tincidunt ante ultrices eget. Vivamus vitae neque at orci congue faucibus vitae sit amet velit. Nam blandit tristique sem id lacinia. Etiam.',
+    buttonLabel: 'Seja um de Nós',
+    imgStart: true,
+    img: require('../../images/svg2.svg').default,
+    alt: 'sushi',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+export const homeObjThree = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Sobre Nós',
+    headLine: 'A Historia do melhor restaurante japones da sua região',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra fermentum nulla,quis tincidunt ante ultrices eget. Vivamus vitae neque at orci congue faucibus vitae sit amet velit. Nam blandit tristique sem id lacinia. Etiam.',
+    buttonLabel: 'Seja um de Nós',
+    imgStart: false,
+    img: require('../../images/svg3.svg').default,
+    alt: 'sushi',
+    dark: true,
+    primary: true,
+    darkText: false
+};
